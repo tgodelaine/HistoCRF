53b8f0d2479e9670e45d126c7fbd4e027fac75b5
Tiffanie Godelaine (tiffanie.godelaine@uclouvain.be) <tgodelai@mbackf2.cism.ucl.ac.be>
2025-09-09 10:22:43 +0200

    cleaning code

--- compatibility.py
+++ compatibility.py
@@ -1,29 +1,29 @@
-import numpy as np
+import torch
 
+device = 'cuda'
 
-def compatibility_from_text_features(args, npz_path):
-    # Load the .npz file
-    data = np.load(npz_path)
-
+def compatibility_from_text_features(args, data):
     # Access the required keys
-    text_features = data['text_features']
+    text_features = torch.tensor(data['text_features'], device=device)
 
-    compatibility = text_features @ text_features.T 
+    compatibility = 1 - text_features @ text_features.T 
 
-    return 1 - compatibility  
+    return compatibility
 
 
-def compatibility_from_potts(args, npz_path):
-    # Load the .npz file
-    data = np.load(npz_path)
-
+def compatibility_from_potts(args, data):
     # Access the required keys
-    n_labels = data['n_labels'] #12
-    #weight = args.weight
+    n_labels = data['n_labels']
 
-    compatibility = 1 - np.identity(n_labels)
+    compatibility = 1 - torch.eye(n_labels, device=device)
 
-    return compatibility#weight * compatibility  
-
+    return compatibility 
 
 
+def compatibility_from_indicator(args, data):
+    # Access the required keys
+    n_labels = data['n_labels']
+
+    compatibility = torch.eye(n_labels, device=device)
+
+    return compatibility
